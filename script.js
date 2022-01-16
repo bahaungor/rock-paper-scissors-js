e65ebf0efa28a09d4c94cb907ab8312d3404c460
Bahadir Ungor <bahadirungor@gmail.com>
2022-01-16 15:27:27 +0300

    project is finished

--- script.js
+++ script.js
@@ -1,85 +1,96 @@
-let PlayerWin = 0;
-let ComputerWin = 0;
-let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let computerHand;
+let playerHand;
+const boxes = document.querySelectorAll(".box");
+const statusBox = document.querySelector(".status");
+const playerScoreBox = document.querySelector(".player-score");
+const computerScoreBox = document.querySelector(".computer-score");
+playerScoreBox.textContent = "0";
+computerScoreBox.textContent = "0";
 
 function computerPlay() {
-    let computerchoice = Math.floor(Math.random() * 3)
-    switch (computerchoice % 3){
+    computerHand = Math.floor(Math.random() * 3);
+    switch (computerHand) {
         case 0:
-            return "ROCK";
+            return computerHand= "rock";
             break;
-        
+    
         case 1:
-            return "PAPER";
+            return computerHand= "paper";
             break;
 
         case 2:
-            return "SCISSORS";
+            return computerHand= "scissors";
             break;
 
         default:
-            return "Something went wrong with computerPlay function. Please refresh the page."
+            break;
     }
 }
 
-function decideWinner (playerSelection, computerSelection){
-    if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "ROCK"){
-        return "Rock vs Rock! It's a tie!";
-    } else if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "PAPER") {
-        PlayerWin++;
-        return "Paper beats Rock! You Win!";
-    } else if (computerSelection == "ROCK" && playerSelection.toUpperCase() == "SCISSORS") {
-        ComputerWin++;
-        return "Rock beats Scissors! You Lose!";
-    } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "ROCK") {
-        ComputerWin++;
-        return "Paper beats Rock! You Lose!";
-    } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "PAPER") {
-        return "Paper vs Paper! It's a tie!";
-    } else if (computerSelection == "PAPER" && playerSelection.toUpperCase() == "SCISSORS") {
-        PlayerWin++;
-        return "Scissors beats Paper! You Win!";
-    } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "SCISSORS") {
-        return "Scissors vs Scissors! It's a tie!";
-    } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "PAPER") {
-        ComputerWin++;
-        return "Scissors beats Paper! You Lose!";
-    } else if (computerSelection == "SCISSORS" && playerSelection.toUpperCase() == "ROCK") {
-        PlayerWin++;
-        return "Rock beats Scissors! You Win!";
+function updateStatus(){
+    if (playerScore == 5) {
+        statusBox.textContent = "You Win! World Is Saved!";
+    } else if (computerScore == 5) {
+        statusBox.textContent = "Computer Win, All Is Lost :(";
     } else {
-        return "Something went wrong with rockPaperScissors function. Please refresh the page.";
+        statusBox.textContent = "Something went wrong!";
     }
 }
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
-const boxes = document.querySelectorAll(".box")
-const computerScore = document.querySelector(".computer-score");
-const playerScore = document.querySelector(".player-score");
-const statusBox = document.querySelector(".status");
-
-computerScore.textContent = ComputerWin;
-playerScore.textContent = PlayerWin;
-let computerSelection;
 
 function endGame(){
-    console.log("this loop is entered")
-    boxes.forEach(box => box.removeEventListener("click", () => {
-        if (ComputerWin == 5 ) statusBox.textContent = "Computers win :(";
-        if (PlayerWin == 5 ) statusBox.textContent = "You win :)";
-    }) );
-} 
-
-boxes.forEach(box => box.addEventListener ("click", function() {
-    computerSelection = computerPlay();
-    statusBox.textContent = decideWinner(this.classList[0], computerPlay());
-    computerScore.textContent = ComputerWin;
-    playerScore.textContent = PlayerWin;
-    console.log("computer win is " + ComputerWin)
-    console.log("player win is " + PlayerWin)
-    if (ComputerWin == 5 || PlayerWin == 5) endGame(); 
-}))
+    boxes.forEach(box => box.classList.remove("clickable"));
+    boxes.forEach(box => box.removeEventListener("click", updateStatus()));
+}
 
 
+function checkRoundWinner(playerHand, computerHand){
+    if (playerHand == computerHand) {
+        statusBox.textContent = "It's a tie";
+    } else if (playerHand == "rock" && computerHand == "paper") {
+        computerScore++;
+        computerScoreBox.textContent = computerScore;
+        statusBox.textContent = "Paper  beats rock! You Lose :(";
+    } else if (playerHand == "rock" && computerHand == "scissors") {
+        playerScore++;
+        playerScoreBox.textContent = playerScore;
+        statusBox.textContent = "Rock beats Scissors! You Win!";
+    } else if (playerHand == "paper" && computerHand == "rock") {
+        playerScore++;
+        playerScoreBox.textContent = playerScore;
+        statusBox.textContent = "Paper  beats rock! You Win!";
+    } else if (playerHand == "paper" && computerHand == "scissors") {
+        computerScore++;
+        computerScoreBox.textContent = computerScore;
+        statusBox.textContent = "Scissors beats paper! You Lose :(";
+    } else if (playerHand == "scissors" && computerHand == "paper") {
+        playerScore++;
+        playerScoreBox.textContent = playerScore;
+        statusBox.textContent = "Scissors beats paper! You Win!";
+    } else if (playerHand == "scissors" && computerHand == "rock") {
+        computerScore++;
+        computerScoreBox.textContent = computerScore;
+        statusBox.textContent = "Rock beats scissors! You Lose :("
+    } else {
+        return "Something went wrong. Please refresh the page.";
+    }
+    if (playerScore == 5 || computerScore == 5) {
+        endGame();
+    }
+}
 
+function playRound(){
+    if (playerScore == 5 || computerScore == 5) {
+        endGame();
+    } else {
+        checkRoundWinner(playerHand, computerHand);
+    }
+}
+
+
+boxes.forEach(box => box.addEventListener("click", function(e){
+        playerHand = this.classList[0];
+        computerPlay();
+        playRound();
+}));
